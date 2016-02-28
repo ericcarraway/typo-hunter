35b342f66dbfd519dd2506540a713ac3ca35f110
Eric Lee Carraway <github@ericcarraway.com>
2016-02-27 22:01:59 -0600

    fixes script locations, adds .gitignore

--- site/js/textUtil.js
+++ site/js/textUtil.js
@@ -1,6 +1,6 @@
-var textUtil = {};
+var textUtil = textUtil || {};
 
-textUtil.uniqueWords = function(str) {
+textUtil.uniqueWords = function (str) {
     // remove line breaks
     str = str.replace(/(\r\n|\n|\r)/gm, ' ');
 
@@ -25,18 +25,85 @@ textUtil.uniqueWords = function(str) {
     return returnArr.sort().join('\n');
 };
 
-var domManip = {};
+textUtil.filterCommonProse = function (str) {
+    // remove line breaks
+    str = str.replace(/(\r\n|\n|\r)/gm, ' ');
 
-domManip.getTextarea = function () {
-    return document.getElementsByName('textarea')[0].value;
+    // split words
+    var arr = str.split(' ');
+
+    // trim words
+    for (var word = 0; word < arr.length; word++) {
+        arr[word] = arr[word].trim();
+    }
+
+    // bring together word lists
+    // currently 4,179 unique words
+    var prose = [].concat(
+        wordlists.deekayen,
+        wordlists.splasho,
+        wordlists.aspell
+    );
+
+    var returnArr = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        // if the word in question does not exist in the array,
+        // add it to the array
+        if (prose.indexOf(arr[i]) === -1) {
+            returnArr.push(arr[i]);
+        }
+    }
+
+    return returnArr.sort().join('\n');
 };
 
-domManip.setTextarea = function (str) {
-    document.getElementsByName('textarea')[0].value = str;
+textUtil.filterTmProse = function (str) {
+    // remove line breaks
+    str = str.replace(/(\r\n|\n|\r)/gm, ' ');
+
+    // split words
+    var arr = str.split(' ');
+
+    // trim words
+    for (var word = 0; word < arr.length; word++) {
+        arr[word] = arr[word].trim();
+    }
+
+    var returnArr = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        // if the word in question does not exist in the array,
+        // add it to the array
+        if (wordlists.motto.indexOf(arr[i]) === -1) {
+            returnArr.push(arr[i]);
+        }
+    }
+
+    return returnArr.sort().join('\n');
 };
 
-function listUniqueWords() {
-    var words = domManip.getTextarea();
-    words = textUtil.uniqueWords(words);
-    domManip.setTextarea(words);
-}
+textUtil.filterAlice = function (str) {
+    // remove line breaks
+    str = str.replace(/(\r\n|\n|\r)/gm, ' ');
+
+    // split words
+    var arr = str.split(' ');
+
+    // trim words
+    for (var word = 0; word < arr.length; word++) {
+        arr[word] = arr[word].trim();
+    }
+
+    var returnArr = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        // if the word in question does not exist in the array,
+        // add it to the array
+        if (wordlists.alice.indexOf(arr[i]) === -1) {
+            returnArr.push(arr[i]);
+        }
+    }
+
+    return returnArr.sort().join('\n');
+};
