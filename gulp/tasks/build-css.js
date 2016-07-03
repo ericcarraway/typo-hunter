07b12b13d013ed6ed76ba4d2aefc34c9265b7844
Eric Lee Carraway <github@ericcarraway.com>
2016-07-03 14:54:03 -0500

    build-css: adds css minification via gulp-cssnano

--- gulp/tasks/build-css.js
+++ gulp/tasks/build-css.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
 
 // compile *.scss files
 gulp.task('build-css', function () {
@@ -9,9 +10,10 @@ gulp.task('build-css', function () {
         'source/scss/**/*.scss',
         'source/scss/**/*.css'
         ])
-        .pipe(sourcemaps.init())  // process the original sources
-        .pipe(concat('styles.css'))
-        .pipe(sass())
-        .pipe(sourcemaps.write()) // add the map to modified source
+        .pipe(sourcemaps.init())    // process the original sources
+        .pipe(concat('styles.css')) // concanate to one file: 'styles.css'
+        .pipe(sass())               // convert sass to css
+        .pipe(cssnano())            // minify css
+        .pipe(sourcemaps.write())   // add the map to the modified source
         .pipe(gulp.dest('dist'));
 });
