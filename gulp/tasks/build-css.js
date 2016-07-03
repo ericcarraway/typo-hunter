83ba3bf991e46837bbf852965d7ba697f2013f29
Eric Lee Carraway <github@ericcarraway.com>
2016-07-03 14:30:58 -0500

    build-css: adds concatenation

--- gulp/tasks/build-css.js
+++ gulp/tasks/build-css.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 
 // compile *.scss files
 gulp.task('build-css', function () {
     return gulp.src('source/scss/**/*.scss')
         .pipe(sourcemaps.init())  // process the original sources
+        .pipe(concat('styles.css'))
         .pipe(sass())
         .pipe(sourcemaps.write()) // add the map to modified source
         .pipe(gulp.dest('dist'));
