786c86afc866ae308e1bda68bc9c170a038669d0
Eric Lee Carraway <github@ericcarraway.com>
2016-07-04 11:30:32 -0500

    build: remove gulp-sourcemaps

--- gulp/tasks/build-js.js
+++ gulp/tasks/build-js.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var gutil = require('gulp-util');
 var uglify = require('gulp-uglify');
@@ -10,12 +9,10 @@ gulp.task('build-js', function () {
         'source/scripts/**/*.js',
         '!source/scripts/**/*.spec.js' // exclude tests
         ])
-        .pipe(sourcemaps.init())
         .pipe(concat('bundle.js'))
 
         // run this command to minify/uglify:
         // $ gulp --type production
         .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
-        .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist'));
 });
