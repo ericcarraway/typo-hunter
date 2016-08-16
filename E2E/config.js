8ed1ddcf9da64197cf28e9cf5ff7e3dd93135f8f
Eric Lee Carraway <github@ericcarraway.com>
2016-08-15 21:37:59 -0500

    update E2E; add config.js

---
+++ E2E/config.js
@@ -0,0 +1,7 @@
+module.exports = (function () {
+    var baseUrl = 'http://typo-hunter.com';
+
+    return {
+        baseUrl: baseUrl
+    };
+}());
--- E2E/features/00-initial-chimp-test.js
+++ E2E/features/00-initial-chimp-test.js
@@ -1,21 +1,69 @@
-// 2016-08-13
+var config = require('../config.js');
 
-// https://chimp.readme.io/
-// https://github.com/xolvio/chimp
+describe('initial tests', function () {
+    before(function () {
+        browser.url(config.baseUrl);
+    });
 
-// $ cd typo-hunter/E2E
+    it('should have the correct title', function () {
+        var title = browser.getTitle();
 
-// $ chimp --mocha --browser=firefox
+        expect(title).to.equal('typo-hunter.com');
+    });
 
-describe('initial Chimp.js test', function () {
-    describe('loading the main page', function () {
-        // example from the documentation:
-        it('should have the correct title', function () {
-            var title;
+    it('should have 17 buttons', function () {
+        var selector = 'button';
+        var elements = browser.elements(selector);
 
-            browser.url('http://typo-hunter.com');     // set up
-            title = browser.getTitle();                // execute
-            expect(title).to.equal('typo-hunter.com'); // verify
+        expect(elements.value.length).to.equal(17);
+    });
+
+    it('should have the correct button text', function () {
+        var selector = 'button';
+        var arrayBtnTxtActual;
+        var arrayBtnTxtExpected = [
+            'list unique words',
+            'filter common prose',
+            'remove punctuation',
+            'trim',
+            'TM',
+            'alice.txt',
+            '2016-04-07',
+            '2016-04-25',
+            'JP SG',
+            'air',
+            'angular docs',
+            'rxPageObjects',
+            '2016-05-14',
+            'newline to space',
+            'remove empty lines',
+            'GH PR',
+            'remove numbers'
+        ];
+
+        arrayBtnTxtActual = browser.getText(selector);
+
+        expect(arrayBtnTxtActual).to.eql(arrayBtnTxtExpected);
+    });
+
+    it('should have a text area', function () {
+        var selector = 'textarea';
+        var doesExist = browser.waitForExist(selector);
+
+        expect(doesExist).to.be.true;
+    });
+
+    describe('github-fork-ribbon', function () {
+        var selector = 'a.github-fork-ribbon';
+
+        it('should exist', function () {
+            var doesExist = browser.waitForExist(selector);
+            expect(doesExist).to.be.true;
+        });
+
+        it('should have the correct text', function () {
+            var ribbonText = browser.getText(selector);
+            expect(ribbonText).to.equal('Fork me on GitHub');
         });
     });
 });
