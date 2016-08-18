f37ad215fcbcf4c28de68c07619e3a8f5ccf62da
Eric Lee Carraway <github@ericcarraway.com>
2016-08-17 23:00:42 -0500

    adds isolateCamelCase and isolatePascalCase

---
+++ source/scripts/pure-functions/isolateCamelCase/isolateCamelCase.spec.js
@@ -0,0 +1,117 @@
+var isolateCamelCase = require('./isolateCamelCase.js');
+var expect = require('chai').expect;
+
+describe('isolateCamelCase', function () {
+    var input;
+    var actual;
+    var expected;
+
+    describe('smoke tests', function () {
+        it('should exist', function () {
+            expect(isolateCamelCase).to.exist;
+        });
+
+        it('should be a function', function () {
+            expect(isolateCamelCase).to.be.a('function');
+        });
+
+        it('should return an array', function () {
+            input = ['this', 'is', 'a', 'test'];
+            expect(isolateCamelCase(input)).to.be.an('array');
+        });
+    });
+
+    describe('tests', function () {
+        it('initial test', function () {
+            input = ['the', 'quick', 'brownFox', 'jumpsOver', 'the', 'lazyDog'];
+            actual = isolateCamelCase(input);
+            expected = ['brownFox', 'jumpsOver', 'lazyDog'];
+
+            expect(actual).to.eql(expected);
+        });
+
+        it('should match a camelCase word', function () {
+            input = ['fooBar', 'control'];
+            actual = isolateCamelCase(input);
+            expected = ['fooBar'];
+
+            expect(actual).to.eql(expected);
+        });
+
+        it('should match camelCase words with multiple humps', function () {
+            input = ['fooBarFizzBuzz', 'control'];
+            actual = isolateCamelCase(input);
+            expected = ['fooBarFizzBuzz'];
+
+            expect(actual).to.eql(expected);
+        });
+    });
+
+    describe('negative tests', function () {
+        it('should not match PascalCase words [1]', function () {
+            input = ['DoNotMatchThis'];
+            actual = isolateCamelCase(input);
+            expected = [];
+
+            expect(actual).to.eql(expected);
+        });
+
+        it('should not match PascalCase words [2]', function () {
+            input = ['DoNotMatchThis', 'FooBar', 'control', 'bazQux'];
+            actual = isolateCamelCase(input);
+            expected = ['bazQux'];
+
+            expect(actual).to.eql(expected);
+        });
+
+        it('should not match Capital words [1]', function () {
+            input = ['Foobar'];
+            actual = isolateCamelCase(input);
+            expected = [];
+
+            expect(actual).to.eql(expected);
+        });
+
+        it('should not match Capital words [2]', function () {
+            input = ['Foobar', 'control', 'bazQux'];
+            actual = isolateCamelCase(input);
+            expected = ['bazQux'];
+
+            expect(actual).to.eql(expected);
+        });
+
+        it('should not match lowercase words [1]', function () {
+            input = ['foobar'];
+            actual = isolateCamelCase(input);
+            expected = [];
+
+            expect(actual).to.eql(expected);
+        });
+
+        it('should not match lowercase words', function () {
+            input = ['foobar', 'control', 'bazQux'];
+            actual = isolateCamelCase(input);
+            expected = ['bazQux'];
+
+            expect(actual).to.eql(expected);
+        });
+    });
+
+    describe('edge cases', function () {
+        it('should handle an empty array', function () {
+            input = [];
+            actual = isolateCamelCase(input);
+            expected = [];
+
+            expect(actual).to.eql(expected);
+        });
+
+        it('should handle an array with empty strings', function () {
+            input = ['', '', ''];
+            actual = isolateCamelCase(input);
+            expected = [];
+
+            expect(actual).to.eql(expected);
+        });
+    });
+});
---
+++ source/scripts/pure-functions/isolatePascalCase/isolatePascalCase.spec.js
@@ -0,0 +1,117 @@
+var isolatePascalCase = require('./isolatePascalCase.js');
+var expect = require('chai').expect;
+
+describe('isolatePascalCase', function () {
+    var input;
+    var actual;
+    var expected;
+
+    describe('smoke tests', function () {
+        it('should exist', function () {
+            expect(isolatePascalCase).to.exist;
+        });
+
+        it('should be a function', function () {
+            expect(isolatePascalCase).to.be.a('function');
+        });
+
+        it('should return an array', function () {
+            input = ['this', 'is', 'a', 'test'];
+            expect(isolatePascalCase(input)).to.be.an('array');
+        });
+    });
+
+    describe('tests', function () {
+        it('initial test', function () {
+            input = ['the', 'quick', 'BrownFox', 'JumpsOver', 'the', 'LazyDog'];
+            actual = isolatePascalCase(input);
+            expected = ['BrownFox', 'JumpsOver', 'LazyDog'];
+
+            expect(actual).to.eql(expected);
+        });
+
+        it('should match a PascalCase word', function () {
+            input = ['FooBar', 'control'];
+            actual = isolatePascalCase(input);
+            expected = ['FooBar'];
+
+            expect(actual).to.eql(expected);
+        });
+
+        it('should match PascalCase words with multiple humps', function () {
+            input = ['FooBarFizzBuzz', 'control'];
+            actual = isolatePascalCase(input);
+            expected = ['FooBarFizzBuzz'];
+
+            expect(actual).to.eql(expected);
+        });
+    });
+
+    describe('negative tests', function () {
+        it('should not match camelCase words [1]', function () {
+            input = ['doNotMatchThis'];
+            actual = isolatePascalCase(input);
+            expected = [];
+
+            expect(actual).to.eql(expected);
+        });
+
+        it('should not match camelCase words [2]', function () {
+            input = ['doNotMatchThis', 'fooBar', 'control', 'BazQux'];
+            actual = isolatePascalCase(input);
+            expected = ['BazQux'];
+
+            expect(actual).to.eql(expected);
+        });
+
+        it('should not match Capital words [1]', function () {
+            input = ['Foobar'];
+            actual = isolatePascalCase(input);
+            expected = [];
+
+            expect(actual).to.eql(expected);
+        });
+
+        it('should not match Capital words [2]', function () {
+            input = ['Foobar', 'control', 'BazQux'];
+            actual = isolatePascalCase(input);
+            expected = ['BazQux'];
+
+            expect(actual).to.eql(expected);
+        });
+
+        it('does not match lowercase words [1]', function () {
+            input = ['foobar'];
+            actual = isolatePascalCase(input);
+            expected = [];
+
+            expect(actual).to.eql(expected);
+        });
+
+        it('does not match lowercase words [2]', function () {
+            input = ['foobar', 'control', 'BazQux'];
+            actual = isolatePascalCase(input);
+            expected = ['BazQux'];
+
+            expect(actual).to.eql(expected);
+        });
+    });
+
+    describe('edge cases', function () {
+        it('handles an empty array', function () {
+            input = [];
+            actual = isolatePascalCase(input);
+            expected = [];
+
+            expect(actual).to.eql(expected);
+        });
+
+        it('handles an array with empty strings', function () {
+            input = ['', '', ''];
+            actual = isolatePascalCase(input);
+            expected = [];
+
+            expect(actual).to.eql(expected);
+        });
+    });
+});
