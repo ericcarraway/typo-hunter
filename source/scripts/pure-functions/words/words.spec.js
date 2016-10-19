8e3e17ecf0e2bfa682eeaa078eda661708f3706e
Eric Lee Carraway <github@ericcarraway.com>
2016-10-18 20:28:06 -0500

    test(words.spec.js): test Lodash / _.words function

--- source/scripts/pure-functions/words/words.spec.js
+++ source/scripts/pure-functions/words/words.spec.js
@@ -6,6 +6,8 @@
 const _ = require('lodash');
 const expect = require('chai').expect;
 
+// consider RegEx and punctuation
+// excludes commas, includes periods and quotation marks
 describe('words', function () {
     var input;
     var actual;
@@ -15,7 +17,7 @@ describe('words', function () {
         expect(_.words).to.be.a('function');
     });
 
-    it('should convert a string to an array of words', function () {
+    it('should convert a string to an array of words (1)', function () {
         input = "Why, sometimes I've believed as many as six impossible things before breakfast.";
         actual = _.words(input, /[^, ]+/g);
         expected = [
@@ -31,6 +33,45 @@ describe('words', function () {
             'things',
             'before',
             'breakfast.'
+        ];
+
+        expect(actual).to.eql(expected);
+    });
+
+    it('should convert a string to an array of words (2)', function () {
+        input = 'Fourscore and seven years ago our fathers brought forth, upon this continent, a new nation, ' +
+                'conceived in liberty and dedicated to the proposition that "all men are created equal."';
+        actual = _.words(input, /[^, ]+/g);
+        expected = [
+            'Fourscore',
+            'and',
+            'seven',
+            'years',
+            'ago',
+            'our',
+            'fathers',
+            'brought',
+            'forth',
+            'upon',
+            'this',
+            'continent',
+            'a',
+            'new',
+            'nation',
+            'conceived',
+            'in',
+            'liberty',
+            'and',
+            'dedicated',
+            'to',
+            'the',
+            'proposition',
+            'that',
+            '\"all',
+            'men',
+            'are',
+            'created',
+            'equal.\"'
         ];
 
         expect(actual).to.eql(expected);
