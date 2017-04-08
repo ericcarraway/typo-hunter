5973756917ccfb666f811fc1922ff2a8e3700a7c
Eric Lee Carraway <github@ericcarraway.com>
2017-04-08 10:35:44 -0500

    modularize(prettyPrintArrOfWords): available to Node.js and browser

--- source/scripts/pure-functions/prettyPrintArrOfWords/prettyPrintArrOfWords.js
+++ source/scripts/pure-functions/prettyPrintArrOfWords/prettyPrintArrOfWords.js
@@ -1,15 +1,30 @@
-function prettyPrintArrOfWords(arr) {
-    var lastIndex;
-    var i;
-    var returnStr = 'var words = [\n';
-    if (arr.length) {
-        lastIndex = arr.length - 1;
-        for (i = 0; i < lastIndex; i++) {
-            returnStr += "    '" + arr[i] + "',\n";
+(function () {
+    'use strict';
+    var root = this;
+
+    function prettyPrintArrOfWords(arr) {
+        var lastIndex;
+        var i;
+        var returnStr = 'var words = [\n';
+        if (arr.length) {
+            lastIndex = arr.length - 1;
+            for (i = 0; i < lastIndex; i++) {
+                returnStr += "    '" + arr[i] + "',\n";
+            }
+            // no comma after the final word
+            returnStr += "    '" + arr[lastIndex] + "'\n";
         }
-        // no comma after the final word
-        returnStr += "    '" + arr[lastIndex] + "'\n";
+        returnStr += '];';
+        return returnStr;
     }
-    returnStr += '];';
-    return returnStr;
-}
+
+    // boilerplate: make prettyPrintArrOfWords available to both Node.js and the browser
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = prettyPrintArrOfWords;
+        }
+        exports.prettyPrintArrOfWords = prettyPrintArrOfWords;
+    } else {
+        root.prettyPrintArrOfWords = prettyPrintArrOfWords;
+    }
+}).call(this);
